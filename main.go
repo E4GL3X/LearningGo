21685f35c676f8e03ec9dba7f8e3511025d1bd5e
Toha Siddique <toha@alistlimited.com>
2026-03-29 11:47:09 +0600

    Test

--- main.go
+++ main.go
@@ -2,5 +2,16 @@ package main
 import "fmt"
 
 func main() {
-	var x int = 10
+	var x = true
+	fmt.Println(x)
+	var y = false
+	fmt.Println(y)
+	var z = 10
+	fmt.Println(z)
+	var a = 10.05
+	fmt.Println(a)
+	var b = "Hello World!!"
+	fmt.Println(b)
+	var c = 'b'
+	fmt.Println(c)
 }
